0f94556027f58409b54227000b6eb48cb6d2b06c
jht6 <juhaotian@foxmail.com>
2023-02-13 00:19:05 +0800

    test: 创建发布项API测试

--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http/httptest"
 	"os"
 	"testing"
@@ -10,6 +12,12 @@ import (
 )
 
 var router *gin.Engine
+
+func TestMain(m *testing.M) {
+	os.Setenv("SERVER_ENV", "test")
+	router = setupRouter()
+	m.Run()
+}
 
 func TestGet503(t *testing.T) {
 	req := httptest.NewRequest("GET", "/", nil)
@@ -22,8 +30,40 @@ func TestGet503(t *testing.T) {
 	assert.Equal(t, 503, w.Code, "Get 503 when the requested publish doesn't exist")
 }
 
-func TestMain(m *testing.M) {
-	os.Setenv("SERVER_ENV", "test")
-	router = setupRouter()
-	m.Run()
+// ====== api:publish start
+
+func TestApiCreatePublish(t *testing.T) {
+	testName := "test1"
+	testDomain := "test1.es.com"
+	testEntry := "http://localhost:8080/html/b.html"
+	body := gin.H{
+		"name":   testName,
+		"domain": testDomain,
+		"entry":  testEntry,
+	}
+	jsonByte, _ := json.Marshal(body)
+	req := httptest.NewRequest("POST", "/api/create_publish", bytes.NewReader(jsonByte))
+	w := httptest.NewRecorder()
+
+	req.Header.Set("content-type", "application/json")
+
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code, "Http code should be 200")
+
+	var res map[string]interface{}
+	err := json.Unmarshal(w.Body.Bytes(), &res)
+
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 0, int(res["code"].(float64)))
+	assert.Equal(t, testName, res["data"].(map[string]any)["name"])
+	assert.Equal(t, testDomain, res["data"].(map[string]any)["domain"])
+	assert.Equal(t, testEntry, res["data"].(map[string]any)["entry"])
+
 }
+
+func TestApiUpdatePublish(t *testing.T) {
+	// TODO
+}
+
+// ====== api:publish end
